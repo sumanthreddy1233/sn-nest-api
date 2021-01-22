4906afe1b570bbd79f97372a294ec0ac93b8f7ba
sumanthreddy1233 <sumanthreddy.naidu@gmail.com>
2021-01-21 23:51:19 -0600

    mappedtypes module add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { QuestModule } from './quest/quest.module';
+import { PartialType } from '@nestjs/mapped-types';
 @Module({
-  imports: [],
+  imports: [QuestModule],
   controllers: [AppController],
   providers: [AppService],
 })
